b0b23e6a4c868758e9126f6a07ec549786dc3123
Warren Watson <wwatson4506@gmail.com>
2023-11-22 15:53:04 -0800

    Update IUSBEnumeratorEX.cpp
    Fix include name capitalization

--- src/IUSBEnumeratorEX.cpp
+++ src/IUSBEnumeratorEX.cpp
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#include "IUSBEnumeratorEx.h"
+#include "IUSBEnumeratorEX.h"
 #include <LibPrintf.h>
 #include <MemoryHexDump.h>
 #include <GIGA_digitalWriteFast.h>
